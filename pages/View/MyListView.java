2db359d96b8b1c5d8e599323defe12b692c6368c
tetsu <kepokeke1059@gmail.com>
2024-05-12 23:31:09 +0900

    リストのボタンを認識させるところ以外は要件を満たしているはず

--- pages/View/MyListView.java
+++ pages/View/MyListView.java
@@ -4,35 +4,66 @@ import model.data.Head;
 import model.data.HeadGroup;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class MyListView extends JPanel {
-    public static final List<String> testList = List.of("Apple", "Banana1", "Banana2", "Dog",
-            "Eagle", "Fahrenheit1", "Fahrenheit2", "Internet", "Java1", "Java2", "Java3", "KONAMI1",
-            "KONAMI2", "KONAMI3", "KONAMI4", "zzz");
-    public List<String> myList;
-    public JScrollPane myListScrollPanel;
-    public JButton selectButton = new JButton("Select");
 
-    public MyListView() {
-        this.myList = testList;// 適当に初期化
+  // ダミーデータ
+  public static final List<String> testList =
+      List.of("Apple", "Banana1", "Banana2", "Dog", "Eagle", "Fahrenheit1", "Fahrenheit2",
+          "Internet", "Java1", "Java2", "Java3", "KONAMI1", "KONAMI2", "KONAMI3", "KONAMI4", "zzz");
 
-        setLayout(new BorderLayout());
+  public List<String> myList;
+  public JScrollPane myListScrollPanel;
+  public JButton selectButton = new JButton("View");
+  public JButton addButton = new JButton("Add Document");
 
-        // マイリストをリスト形式で表示
-        JList<String> list = new JList<>(myList.toArray(new String[0]));
-        add(myListScrollPanel = new JScrollPane(list), BorderLayout.CENTER);
-        add(selectButton, BorderLayout.SOUTH);
-    }
+  public MyListView(Head[] initialHeads, HeadGroup[] initialHeadGroups) {
 
-    // マイリストの更新
-    public void updateMyList(Head[] heads, HeadGroup[] headGroups) {
-        JList<String> list = new JList<>(myList.toArray(new String[0]));
-        myListScrollPanel = new JScrollPane(list);
-    }
+    this.myList = testList;// 適当に初期化
+    // this.myList = List.of(initialHeads+initialHeadGroups); //こちらにしたい
 
-    public JButton getSelectButton() {
-        return selectButton;
-    }
+    setLayout(new BorderLayout());
+
+    // マイリストをリスト形式で表示
+    JList<String> list = new JList<>(myList.toArray(new String[0]));
+    add(myListScrollPanel = new JScrollPane(list), BorderLayout.CENTER);
+
+    JPanel bottomPanel = new JPanel(new BorderLayout());
+    JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+    buttonPanel.add(addButton);
+    buttonPanel.add(selectButton);
+    bottomPanel.add(buttonPanel, BorderLayout.CENTER);
+    add(bottomPanel, BorderLayout.SOUTH);
+
+  }
+
+  // マイリストの更新
+  public void updateMyList(Head[] heads, HeadGroup[] headGroups) {
+    remove(myListScrollPanel);
+    String[] headNames = Arrays.stream(heads).map(head -> head.name).toArray(String[]::new);
+    String[] headGroupNames =
+        Arrays.stream(headGroups).map(headGroup -> headGroup.name).toArray(String[]::new);
+    JList<String> list = new JList<>(Stream
+        .concat(Arrays.stream(headNames), Arrays.stream(headGroupNames)).toArray(String[]::new));
+
+    myListScrollPanel = new JScrollPane(list);
+    add(myListScrollPanel, BorderLayout.CENTER);
+    revalidate();
+    repaint();
+  }
+
+
+  // 選択ボタンの取得
+  public JButton getSelectButton() {
+    return selectButton;
+  }
+
+  // ドキュメント追加ボタンの取得
+  public JButton getAddButton() {
+    return addButton;
+  }
 
 }

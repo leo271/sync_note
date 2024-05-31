613ff8f0be961a47bd6e6abf1f15697f5d85e889
risa-hira <rrhiramatt@gmail.com>
2024-05-31 14:58:27 +0900

    windowsで動くようにした

--- .vscode/bin/br_backend.ps1
+++ .vscode/bin/br_backend.ps1
@@ -1,5 +1,4 @@
 # build_and_run_backend.ps1
 Set-Location -Path "backend"
 Get-ChildItem -Path . -Filter "*.class" -Recurse | Remove-Item -Force
-javac Main.java -d classes -cp ../libs/sqlite-jdbc.jar
-java -cp classes:../libs/* backend.Main
+java Main.java -cp ../libs/*
--- .vscode/bin/br_frontend.ps1
+++ .vscode/bin/br_frontend.ps1
@@ -1,5 +1,5 @@
 # build_and_run_backend.ps1
 Set-Location -Path "frontend"
 Get-ChildItem -Path . -Filter "*.class" -Recurse | Remove-Item -Force
-javac Main.java -d classes -cp ../libs/sqlite-jdbc.jar
-java -cp classes:../libs/* backend.Main
+javac Main.java -d classes -encoding UTF-8
+java -cp classes Main

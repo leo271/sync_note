82b32cf6c22d49d8381fa9c1812e59b035781ba1
tetsu <kepokeke1059@gmail.com>
2024-06-25 14:46:12 +0900

    コメント削除

--- frontend/View/DocumentEditor.java
+++ frontend/View/DocumentEditor.java
@@ -1,34 +1,3 @@
-// package view;
-
-// import javax.swing.*;
-// import model.Document;
-// import java.awt.*;
-
-// public class DocumentEditor extends JPanel {
-//   public JTextArea contentArea;
-//   public JButton saveButton;
-//   public JButton deleteButton;
-//   public JButton viewButton;
-
-//   public DocumentEditor(Document document) {
-//     setLayout(new BorderLayout());
-
-//     contentArea = new JTextArea(document.content);
-//     add(new JScrollPane(contentArea), BorderLayout.CENTER);
-
-//     JPanel buttonPanel = new JPanel();
-//     saveButton = new JButton("Save");
-//     buttonPanel.add(saveButton);
-
-//     deleteButton = new JButton("Delete");
-//     buttonPanel.add(deleteButton);
-
-//     viewButton = new JButton("View");
-//     buttonPanel.add(viewButton);
-
-//     add(buttonPanel, BorderLayout.SOUTH);
-//   }
-// }
 
 package view;
 

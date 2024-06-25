2071fea9540dd36e821c575629fa662d94b6b650
risa-hira <rrhiramatt@gmail.com>
2024-06-25 15:28:54 +0900

    changes in DocumentsViewer 2

--- frontend/View/DocumentsViewer.java
+++ frontend/View/DocumentsViewer.java
@@ -10,30 +10,18 @@ import java.util.stream.Stream;
 
 public class DocumentsViewer extends JPanel {
     public List<Document> documents;
-    public JLabel titleLabel;
+    public JLabel titleLabel = new JLabel("Document", SwingConstants.CENTER);
     public JTextArea documentsArea;
     public JButton likeButton = new JButton("♥");
     public JLabel likeLabel = new JLabel("          ♥  :   0");
 
-    // ダミーデータ
-    public static final List<String> testList = List.of("Apple", "Banana1", "Banana2", "Dog",
-            "Eagle", "Fahrenheit1", "Fahrenheit2", "Internet", "Java1", "Java2", "Java3", "KONAMI1",
-            "KONAMI2", "KONAMI3", "KONAMI4", "zzz", "a", "a", "a", "a", "a", "a", "a", "a", "a",
-            "a", "a", "a", "a", "a");
-
-    public List<String> DocumentsList;
-    public JScrollPane DocumentsListScrollPanel;
-
 
     public DocumentsViewer(String[] initialHeads) {
-        this.DocumentsList = testList;
-        // this.myList = List.of(initialHeads+initialHeadGroups); //こちらにしたい
 
         setLayout(new BorderLayout());
 
-        // Documentをリスト形式で表示
-        JList<String> list = new JList<>(DocumentsList.toArray(new String[0]));
-        add(DocumentsListScrollPanel = new JScrollPane(list), BorderLayout.CENTER);
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(titleLabel, BorderLayout.NORTH);
 
         JPanel bottomPanel = new JPanel(new BorderLayout());
         JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
@@ -48,10 +36,6 @@ public class DocumentsViewer extends JPanel {
         this.documents = documents;
 
         setLayout(new BorderLayout());
-
-
-        titleLabel = new JLabel("Documents for ");
-        add(titleLabel, BorderLayout.NORTH);
 
         documentsArea = new JTextArea();
         documentsArea.setEditable(false); // 編集不可

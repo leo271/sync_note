0e56ed5b3f2315b5304d089326a8d97a47c95018
tetsu <kepokeke1059@gmail.com>
2024-06-26 21:39:17 +0900

    検索のVMを実装

--- frontend/viewmodel/SearchViewViewModel.java
+++ frontend/viewmodel/SearchViewViewModel.java
@@ -7,9 +7,15 @@ public class SearchViewViewModel {
 
   public SearchViewViewModel(SearchView searchView, DocumentsViewer documentsViewer,
       HeadGroupExplorer headGroupExplorer, SceneManager sceneManager) {
+
+    // 検索
     searchView.searchButton.addActionListener(e -> {
       var inputWords = searchView.searchField.getText();
-
+      var obtainedHeadGroup = HeadsController.searchHeads(inputWords);
+      sceneManager.switchPanel("headGroupExplorer");
+      headGroupExplorer.setHeadGroup(obtainedHeadGroup.message);
     });
+
+
   }
 }

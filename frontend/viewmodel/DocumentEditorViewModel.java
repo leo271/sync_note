54661fcbba50b46fead8b2417729ca80d39f5587
tetsu <kepokeke1059@gmail.com>
2024-06-26 20:04:43 +0900

    少し追加

--- frontend/viewmodel/DocumentEditorViewModel.java
+++ frontend/viewmodel/DocumentEditorViewModel.java
@@ -1,5 +1,14 @@
 package viewmodel;
 
+import view.DocumentEditor;
+
 public class DocumentEditorViewModel {
+
+  public DocumentEditorViewModel(DocumentEditor documentEditor) {
+
+    documentEditor.saveButton.addActionListener(e -> {
+
+    });
+  }
 
 }
--- frontend/viewmodel/SearchViewViewModel.java
+++ frontend/viewmodel/SearchViewViewModel.java
@@ -1,5 +1,15 @@
 package viewmodel;
 
+import view.*;
+import model.*;
+
 public class SearchViewViewModel {
 
+  public SearchViewViewModel(SearchView searchView, DocumentsViewer documentsViewer,
+      HeadGroupExplorer headGroupExplorer, SceneManager sceneManager) {
+    searchView.searchButton.addActionListener(e -> {
+      var inputWords = searchView.searchField.getText();
+
+    });
+  }
 }

1747dacd2954dfd4e79556b9bb5cb5ea4e80a8e2
tetsu <kepokeke1059@gmail.com>
2024-06-26 20:52:17 +0900

    一旦コミット

--- frontend/viewmodel/HeadGroupExplorerViewModel.java
+++ frontend/viewmodel/HeadGroupExplorerViewModel.java
@@ -1,5 +1,22 @@
 package viewmodel;
 
+import javax.swing.JList;
+import model.DocumentController;
+import view.HeadGroupExplorer;
+import view.SceneManager;
+
 public class HeadGroupExplorerViewModel {
+
+  public HeadGroupExplorerViewModel(HeadGroupExplorer headGroupExplorer, SceneManager scwManager) {
+    // 対象の項目に対してSelectされたとき（View）
+    headGroupExplorer.enterButton.addActionListener(e -> {
+      var selects = (JList<?>) headGroupExplorer.HeadGroupScrolPanel.getViewport().getView(); // リストを取得
+      String selectedValue = (String) selects.getSelectedValue(); // 選択中の項目取得
+
+      var obtainedDocs = DocumentController.getDocuments(selectedValue); // ドキュメント取得
+      documentsViewer.setDocuments(obtainedDocs.message); // ドキュメントをDocViewerにセット
+      sceneManager.switchPanel("documentsViewer"); // 遷移
+    });
+  }
 
 }
